834839e8ba7d8539a353cb2746a60a9eaca61fea
haizman <chaizman@umich.edu>
2014-05-22 21:22:20 -0400

    Next Update

--- run_analysis.R
+++ run_analysis.R
@@ -1,7 +1,7 @@
 # Set the url for the file to download and download
-
 localfilepath = "./UCI HAR Dataset.zip"
 
+# Check to see if the zip file is there. If it is then don't download it.
 if (file.exists(localfilepath)){
         warning("Zip file exists in working directory.")
 } else {
@@ -9,4 +9,8 @@ if (file.exists(localfilepath)){
         download.file(fileurl, "./UCI HAR Dataset.zip")
 }
 
+# Print file time
 file.info(localfilepath)$ctime
+
+## Check out the files
+al <- read.csv("activity_labels.txt",header=F,sep=" ",col.names=c("ID","Class Labels"))
