e66ddf5dc1025db05cab03fef4be7835f5c44b13
haizman <chaizman@umich.edu>
2014-05-23 15:09:54 -0400

    Progress

--- run_analysis.R
+++ run_analysis.R
@@ -1,3 +1,4 @@
+#### Downloading Data
 # Set the url for the file to download and download
 localfilepath = "./UCI HAR Dataset.zip"
 
@@ -12,5 +13,35 @@ if (file.exists(localfilepath)){
 # Print file time
 file.info(localfilepath)$ctime
 
-## Check out the files
-al <- read.csv("activity_labels.txt",header=F,sep=" ",col.names=c("ID","Class Labels"))
+#### File Preparation
+# Load secondary files
+activitylabels <- read.csv("./activity_labels.txt",header=F,sep=" ",col.names=c("id","activitylabel"))
+features <- read.csv("./features.txt",header=F,sep=" ",col.names=c("id","featurelabel"))
+
+# Prep the test file
+subjecttest <- read.table("./test/subject_test.txt",header=F,col.names="subjectid")
+xtest <- read.table("./test/x_test.txt",header=F,col.names=tolower(gsub("[\\,\\(\\)\\.\\-]","",features[,2])))
+ytest <- read.table("./test/y_test.txt",header=F,col.names="activityid")
+test <- cbind("test",subjecttest,ytest,xtest)
+colnames(test)[1] <- "filesource"
+test[1:5,1:7]
+
+# Prep the train file
+subjecttrain <- read.table("./train/subject_train.txt",header=F,col.names="subjectid")
+xtrain <- read.table("./train/x_train.txt",header=F,col.names=tolower(gsub("[\\,\\(\\)\\.\\-]","",features[,2])))
+ytrain <- read.table("./train/y_train.txt",header=F,col.names="activityid")
+train <- cbind("train",subjecttrain,ytrain,xtrain)
+colnames(train)[1] <- "filesource"
+train[1:5,1:7]
+
+# Merge the two files together
+tidy <- rbind(test,train)
+tidy[c(1:5,8000:8004),1:7]
+
+#### Extract Mean & Standard Deviation Measurements
+tidyms <- tidy[,c(1:3,grep("mean.*\\(\\)",features[,2],ignore.case=T)+3,
+        grep("std\\(\\)",features[,2],ignore.case=T)+3)]
+tidyms[c(1:5,8000:8004),1:7]
+
+# Create codebook
+write.csv(colnames(tidyms),"./codebook.csv")
